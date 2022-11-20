051802c7104f1b40f1a0bd111635841f8c07cc1e
Shubham Patel <patelshubham1912@gmail.com>
2022-11-20 11:40:12 -0600

    Initial Commit

---
+++ ContractorCalculatorIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContractorCalculatorIOS
+//
+//  Created by shubham patel on 11/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
